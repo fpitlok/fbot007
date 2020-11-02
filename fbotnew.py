5cddbd17490c3fb0e7fdc0e619ad398084f4069c
Fbot Line (Thailand) <f.pitlok@gmail.com>
2020-11-02 12:04:03 +0700

    update fbotnew.py

--- fbotnew.py
+++ fbotnew.py
@@ -13,7 +13,7 @@ from googletrans import Translator
 botStart = time.time()
 #==============================================================================#
 #line = LINE()
-line = LINE("t3subnoi10@gmail.com","1234ok")
+line = LINE("devil9991t@hotmail.com","1234ok")
 #line = LINE('EulkVm03YPzIy1y9cjb3.TAVOkm2wqPizxdXz1JiGmW.8S00JCP4DRsRTBGHcgh3JJRnGxUeoUlXI2iRelhqwrY=')
 line.log("Auth Token : " + str(line.authToken))
 line.log("Timeline Token : " + str(line.tl.channelAccessToken))
